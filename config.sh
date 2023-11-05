32d7db754abcacdc874ea05c2b14f5a722bd45b6
deathmond1987 <deathmond1987@gmail.com>
2023-11-05 10:57:03 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -79,6 +79,7 @@ add_locale () {
          --answerclean None \
          --mflags \"--noconfirm\" glibc"
     sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+    sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/g' /etc/locale.gen
     locale-gen
 }
 
