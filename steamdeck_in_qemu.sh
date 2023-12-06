b5c7fc713aa3bdf24e651c8634f2b90fa7896638
deathmond1987 <deathmond1987@gmail.com>
2023-12-06 13:52:05 +0300

    Update steamdeck_in_qemu.sh

--- steamdeck_in_qemu.sh
+++ steamdeck_in_qemu.sh
@@ -43,7 +43,7 @@ install_steamos () {
                        -device nvme,drive=drive0,serial=badbeef \
                        -drive if=none,id=drive0,file=steamos.qcow2 \
                        -device virtio-net-pci,netdev=net0 \
-                       -netdev user,id=net0,hostfwd=tcp::55555-:22 & sleep 3
+                       -netdev user,id=net0,hostfwd=tcp::55555-:22 --daemonize
 
     # if thin not first install we need to clear ssh known_hosts
     if grep "127.0.0.1]:55555" /home/"$USER"/.ssh/known_hosts; then
@@ -52,12 +52,14 @@ install_steamos () {
     fi
 
     echo -e "SteamOS installer is loading\n"
-    echo -e "You need change deck users passwd and enable sshd:"
-    echo -e "passwd deck"
-    echo -e "sudo systemctl enable --now sshd"
+    echo "In default qemu now opens new window with SteamOS"
+    echo "In headless qemu you need external vnc to connect to 127.0.0.1:55555"
+    echo  "You need change deck users passwd and enable sshd:"
+    echo  "passwd <<--type password for default user"
+    echo  "sudo systemctl enable --now sshd"
     echo ""
     # give some time to manipulate in vm
-    read -n 1 -s -r -p "Now you need wait till SteamOS is loaded and then press any key to continue"
+    read -n 1 -s -r -p "Press any key to connect ssh. After that agree with new key and enter password"
     echo ""
 
     # executing in vm
@@ -67,44 +69,52 @@ install_steamos () {
         sed -i 's/.*zenity.*/true/g' /home/deck/tools/repair_device.sh
         # we does not want to reboot
         sed -i 's/.*cmd systemctl reboot.*/true/g' /home/deck/tools/repair_device.sh
-#  sudo steamos-readonly disable
-#        sudo sed -i 's/.*mount -o ro.*/mount -o rw "$ROOTFS_DEVICE" $dir/g' /usr/bin/steamos-chroot
-#        sudo cp -a /usr/bin/steamos-chroot /home/deck/
-#sudo sed -i 's/.*mount -o ro.*/mount -o rw "$ROOTFS_DEVICE" $dir/g' /home/deck/steamos-chroot
-#  sudo umount -l /etc
+                # creating sddm config overwrite
         echo -e '[Autologin]\nSession=plasma.desktop' > /home/deck/steamos.conf
         sudo chown root:root /home/deck/steamos.conf
-#  sudo steamos-readonly enable
         # executing install script
         /home/deck/tools/repair_reimage.sh
+                # creating mountpoint
         mkdir /home/deck/mnt
+                # mounting btrfs root A
         sudo mount -o rw /dev/nvme0n1p4 /home/deck/mnt
-        sudo cp -a /home/deck/steamos.conf /home/deck/mnt/etc/sddm.conf.d/
+                # disabling btrfs ro property for A
+        sudo btrfs property set /home/deck/mnt ro false
+                # Copying new sddm config to side A
+                sudo cp -a /home/deck/steamos.conf /home/deck/mnt/etc/sddm.conf.d/
+                # unmounting side A
         sudo umount -l /home/deck/mnt
+                # lazymount await
+        sleep 3
+                # for side B
         sudo mount -o rw /dev/nvme0n1p5 /home/deck/mnt
+                sudo btrfs property set /home/deck/mnt ro false
         sudo cp -a /home/deck/steamos.conf /home/deck/mnt/etc/sddm.conf.d/
         sudo umount -l /home/deck/mnt
-#        sudo /home/deck/steamos-chroot --disk /dev/nvme0n1 --partset A -- 'echo' '[Autologin]' > '/etc/sddm.conf.d/steamos.conf' && 'echo' 'Session=plasma.desktop' >> '/etc/sddm.conf.d/steamos.conf'
-#        sudo /home/deck/steamos-chroot --disk /dev/nvme0n1 --partset B -- 'echo' '[Autologin]' '>' '/etc/sddm.conf.d/steamos.conf' '&&' 'echo' 'Session=plasma.desktop' '>>' '/etc/sddm.conf.d/steamos.conf'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset A -- 'echo' '[Autologin]' '>' '/etc/sddm.conf.d/steamos.conf'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset A -- 'echo' 'Session=plasma.desktop' '>>' '/etc/sddm.conf.d/steamos.conf'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset A -- "steamos-readonly enable'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset B -- 'mount' '-o' 'remount,rw' '/' '/'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset B -- 'echo' '[Autologin]' '>' '/etc/sddm.conf.d/steamos.conf'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset B -- 'echo' 'Session=plasma.desktop' '>>' '/etc/sddm.conf.d/steamos.conf'
-#        sudo steamos-chroot --disk /dev/nvme0n1 --partset B -- "steamos-readonly enable"
-
-echo "
-sudo steamos-chroot --disk /dev/nvme0n1 --partset other -- \"steamos-readonly disable\"
-sudo steamos-chroot --disk /dev/nvme0n1 --partset other -- \"echo [Autologin] > /etc/sddm.conf.d/steamos.conf\"
-sudo steamos-chroot --disk /dev/nvme0n1 --partset other -- \"echo Session=plasma.desktop >> /etc/sddm.conf.d/steamos.conf\"
-sudo steamos-chroot --disk /dev/nvme0n1 --partset other -- \"steamos-readonly enable" > /home/deck/post-install.sh
-
-#       sudo systemctl poweroff
+        sleep 3
+######################################################################################################################################
+## У steamdeck AB ro файловая система btrfs.
+## За атомарное обновление отвечает rauc
+## Так как режим игровой режим steam не работает из коробки - необходимо загружаться в kde
+## Для этого нужно изменять /etc/sddm.conf.d/steamos.conf после каждого обновления в слоте который был обновлен
+## У rauc для этого есть post-install.sh скрипт в котором происходит завершение обновления
+## Можно попробовать внедрить туда код который будет после полного обновления:
+## 1. обновлять на соседнем слоте post-install.sh скрипт чтобы при следующем обновлении заменить новый post-install.sh (survival mode)
+## 2. обновлять на соседнем слоте /etc/sddm.conf.d/steamos.conf чтобы сессия менялась на kde
+## 3. может быть включать sshd по умолчанию
+## Суть проблемы в том что атомарное обновление затирает полностью все изменения в слоте.
+## По умолчанию включается игровой режим который не может загрузиться в qemu и становится неработоспособной
+## Необходимо либо патчить вышеуказанным образом либо включать sshd (который по умолчанию всегда выключен) и вносить правки в sddm вручную
+######################################################################################################################################
+                # without exit - this script is wailing with 255 code
+        nohup sudo bash -c "sleep 3 && systemctl poweroff" &
+        exit
 EOF
 
-
-exit 1
+    while ps -aux | grep qemu | grep steamos 2>&1 >/dev/null ; do
+        echo -e "Looks like VM is not shutdown for now. Waiting to gracefully shutdown..."
+        sleep 3
+    done
     cp "$EDK_PATH"/OVMF_VARS.fd ./OVMF_VARS.fd
 }
 
@@ -115,16 +125,16 @@ run_steamos () {
                        -device VGA,xres=1280,yres=800 \
                        -drive if=pflash,format=raw,readonly=on,file="$EDK_PATH"/OVMF.fd \
                        -drive if=pflash,format=raw,file=./OVMF_VARS.fd \
-                       -drive if=virtio,file=steamos.qcow2 \
+                       -device nvme,drive=drive0,serial=badbeef \
+                       -drive if=none,id=drive0,file=steamos.qcow2 \
+#                       -drive if=virtio,file=steamos.qcow2 \
                        -device virtio-net-pci,netdev=net0 \
-                       -netdev user,id=net0,hostfwd=tcp::55555-:22
+                       -netdev user,id=net0,hostfwd=tcp::55555-:22 --daemonize
 }
 
 if [ -f ./steamos.qcow2 ]; then
     run_steamos
 else
     install_steamos
-    run_steamos
-fi
     run_steamos
 fi
